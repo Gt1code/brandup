0cd53765fdd5fa3b509445d96dc02a1747766b78
Godstime Sunday <sundaygodstimegt1@gmail.com>
2025-12-03 10:55:04 +0100

    backup before proceeding

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import ServicesSection from "./components/ServiceSection";
+import Founder from "./components/Founder";
 import Footer from "./components/Footer";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Hero />
         <About />
         <ServicesSection />
+        <Founder />
       </main>
       <Footer />
     </div>
---
+++ src/components/Founder.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+
+function Founder() {
+  return (
+    <article
+      id="founder"
+      className="flex min-h-[calc(100vh-56px)] w-full scroll-mt-15 items-center justify-center bg-[url('/asset/founder-bg.jpg')] bg-cover bg-center bg-no-repeat p-4 text-white md:scroll-mt-14"
+    >
+      <div className="max-w-5/6 md:max-w-4xl">
+        <h2 className="font-hero mb-12 pt-10 text-center text-3xl font-bold text-[#203959] md:text-4xl">
+          Meet The Founder
+        </h2>
+
+        <section className="flex flex-col-reverse gap-8 md:flex-row">
+          <div className="md:flex-1">
+            <p>
+              Mr. Wilson is the Founder and Chief Executive Officer of The
+              Corporate BrandUP Ltd, a business development and corporate
+              support firm dedicated to helping entrepreneurs and organizations
+              start, structure, and grow their businesses with clarity and
+              confidence. With a strong background in business administration,
+              brand development, and corporate advisory, Mr Wilson has built a
+              reputation for simplifying complex processes and providing
+              practical solutions that empower business owners.
+            </p>
+            <p className="mt-4">
+              Under his leadership, The Corporate BrandUP Ltd has become known
+              for its professionalism, transparent processes, and client focused
+              service delivery.
+            </p>
+            <p className="mt-4">
+              Driven by a passion for entrepreneurship, Mr. Wilson is committed
+              to helping individuals turn their ideas into legally established
+              and well structured businesses. He leads with integrity,
+              innovation, and a clear vision to make business setup and
+              development more accessible, stress free, and efficient for people
+              at all stages of their business journey.
+            </p>
+          </div>
+
+          <figure className="flex flex-col items-center md:flex-1">
+            <img
+              src="/asset/profile-pic.png"
+              alt="Founder's Profile Picture"
+              className="h-auto w-full max-w-[200px] -skew-1 rounded-4xl sm:max-w-[250px] md:max-w-[300px]"
+            />
+            <figcaption className="text-center">
+              <h3 className="font-bold text-[#008576]">Efe Wilson Aghwata</h3>
+              <p>Founder / CEO</p>
+            </figcaption>
+          </figure>
+        </section>
+      </div>
+    </article>
+  );
+}
+
+export default Founder;

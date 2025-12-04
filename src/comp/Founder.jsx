510700e55341e6416beaf954bb18c0fbee534dca
Godstime Sunday <sundaygodstimegt1@gmail.com>
2025-12-04 22:40:43 +0100

    optimized images

--- src/comp/Founder.jsx
+++ src/comp/Founder.jsx
@@ -56,7 +56,7 @@ function Founder() {
 
           <figure className="flex flex-col items-center md:flex-1">
             <img
-              src="/asset/profile-pic.png"
+              src="/asset/profile-pic-min.jpg"
               alt="Founder's Profile Picture"
               className="h-auto w-full max-w-[200px] rounded-4xl sm:max-w-[250px] md:max-w-[300px]"
             />
